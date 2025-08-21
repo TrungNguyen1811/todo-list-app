86def2cba8758817a5a6485423b6aeaa9bd4e6eb
TrungNguyen1811 <trung4atlam@gmail.com>
2025-08-21 08:42:35 +0700

    feat: drag and drop

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -25,6 +25,14 @@ const router = createBrowserRouter([
           </Suspense>
         ),
       },
+      {
+        path: '/tasks',
+        element: (
+          <Suspense fallback={<div>Loading page...</div>}>
+            <HomePage />
+          </Suspense>
+        ),
+      },
     ],
   },
   {
