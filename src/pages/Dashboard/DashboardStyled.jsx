d87da2c765bf710cb2b67e2d40c142b14464e4f7
hdongqt <hdongqt@gmail.com>
2025-08-26 08:32:55 +0700

    feat: "add custom hook dashboard"

---
+++ src/pages/Dashboard/DashboardStyled.jsx
@@ -0,0 +1,8 @@
+import { Layout } from 'antd'
+import { styled } from 'styled-components'
+
+const LayoutDashboardStyled = styled(Layout)`
+  min-height: 100vh;
+`
+
+export { LayoutDashboardStyled }
